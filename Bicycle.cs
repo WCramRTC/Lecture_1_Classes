cb6bb5ebf953a596805b8369a8c896521324664c
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-05 19:59:02 -0700

    Initial commit

---
+++ Bicycle.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1_Classes
+{
+    public class Bicycle
+    {
+        enum TypesOfChains { Chain1, Chain2, Chain3 }
+        // Fields
+        string _breaks;
+        TypesOfChains  _chain;
+        string _typeOfWheels;
+        int _numberOfGears;
+        string _frame;
+        string _pedals;
+        string _handle;
+        int _numberOfSeats;
+        string _uniqueSerial;
+
+        // quICK ACTION
+        // click on light bulb
+        // Right click and selected
+        // alt + enter
+        // ctrl + .
+
+
+
+        // Constructor
+        public Bicycle(int numberOfSeats)
+        {
+            Random rand = new Random();
+            int unique = rand.Next(10000000, 100000000);
+            _uniqueSerial = unique.ToString();
+            NumberOfSeats = numberOfSeats;
+        }
+
+
+
+
+
+
+        // Properties
+        public string UniqueSerial
+        {
+            //get
+            //{
+            //    return _uniqueSerial;
+            //}
+            get => _uniqueSerial;
+        }
+     
+        public int NumberOfSeats
+        {
+            get => _numberOfSeats;
+            set
+            {
+                if(value >= 0)
+                {
+                    _numberOfSeats = value;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a postive value");
+                }
+            }
+        }
+
+        public string Breaks { get => _breaks; set => _breaks = value; }
+        private TypesOfChains Chain { get => _chain; set => _chain = value; }
+        public string TypeOfWheels { get => _typeOfWheels; set => _typeOfWheels = value; }
+        public int NumberOfGears { get => _numberOfGears; set => _numberOfGears = value; }
+        public string Frame { get => _frame; set => _frame = value; }
+        public string Pedals { get => _pedals; set => _pedals = value; }
+        public string Handle { get => _handle; set => _handle = value; }
+        public int NumberOfSeats1 { get => _numberOfSeats; set => _numberOfSeats = value; }
+        public string UniqueSerial1 { get => _uniqueSerial; set => _uniqueSerial = value; }
+
+
+        // Methods
+
+
+        public override string ToString()
+        {
+            return $"Unique Serial: {UniqueSerial}";
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lecture_1_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates a new instance of our Bicycle class
+            // What is an instance
+            Bicycle bicycle = new Bicycle(-1);
+            Bicycle bicycle2 = new Bicycle(-1)
+            {
+                // ctrl + space brings up intellisense
+                Breaks = "Breaks",
+                Pedals = "Pedals"
+            };
+
+        } // Main
+    } // class
+
+} // namespace
